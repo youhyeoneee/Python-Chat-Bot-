7eaa183df6818faa3cec065a545cb9b70861cdd3
김유현 <khukyh2018@khu.ac.kr>
2018-08-14 19:48:32 +0900

    주석 달기

--- 00.basic/telegram_bot2.py
+++ 00.basic/telegram_bot2.py
@@ -1,6 +1,6 @@
 import telegram
 
-
+#채널에 메세지 보내기
 my_token ='602009043:AAHbtbRjehdhptkyxyxoyYbZdhd_RwYwyNw'
 bot = telegram.Bot(token = my_token)
 bot.sendMessage(chat_id = '@dasom1', text="저는 봇입니다.")
