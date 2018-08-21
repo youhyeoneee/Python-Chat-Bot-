40bb5b627b0194d9f3857c51f6dcfb97d6b68279
김유현 <khukyh2018@khu.ac.kr>
2018-08-22 00:08:27 +0900

    Add target_list

--- 07.everytime_keyword_bot/main.py
+++ 07.everytime_keyword_bot/main.py
@@ -5,7 +5,12 @@ import time
 
 ID = CONFIG.ID
 PW = CONFIG.PW
+TARGET_KEYWORD_LIST = CONFIG.TARGET_KEYWORD_LIST
+#찾고자 하는 키워드 추가
+keyword = input(">>알림받을 키워드를 입력해주세요 ::")
+TARGET_KEYWORD_LIST.append(keyword)
 
+#브라우저 제어
 browser = webdriver.Chrome('./chromedriver')
 browser.maximize_window()
 
@@ -26,13 +31,14 @@ oldList = []
 while True:
     #페이지 소스 얻어오기
     bs4 = BeautifulSoup(browser.page_source, 'lxml')
-
+    print(TARGET_KEYWORD_LIST)
     # 알림 부분 구현
     articles = bs4.find_all('article')
     for article in articles:
-        if CONFIG.TARGET_KEYWORD in article.h2.get_text().strip() and article.h2.get_text().strip() not in oldList:
-            print("[알림] :: ",article.h2.get_text().strip())
-            oldList.append(article.h2.get_text().strip())
+        for target_keyword in TARGET_KEYWORD_LIST:
+            if target_keyword in article.h2.get_text().strip() and article.h2.get_text().strip() not in oldList:
+                print("[알림] :: ",article.h2.get_text().strip())
+                oldList.append(article.h2.get_text().strip())
 
     #새로고침후 원하는 딜레이시간까지 기다리기
     browser.get('https://khu.everytime.kr/370463')
