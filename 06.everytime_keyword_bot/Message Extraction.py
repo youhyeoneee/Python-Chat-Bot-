ba84fdd04168693a7f0e64e3371fe17d3479ad6c
김유현 <khukyh2018@khu.ac.kr>
2018-08-26 00:54:51 +0900

    메세지 추출 기능

---
+++ 06.everytime_keyword_bot/Message Extraction.py
@@ -0,0 +1,14 @@
+import telegram
+import CONFIG
+
+TOKEN = CONFIG.BOT_TOKEN
+CHAT_ID = CONFIG.CHAT_ID
+
+BOT_URL = "https://api.telegram.org/bot{}/sendMessage?chat_id={}".format(TOKEN, CHAT_ID)
+BOT = telegram.Bot(token = TOKEN) # bot 선언
+
+updates = BOT.getUpdates(chat_id= CHAT_ID)
+
+for u in updates:
+    print(u)
+    print(u.channel_post.text)
