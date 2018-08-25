0350966af3a78489ab3df143e46d0c797f630339
김유현 <khukyh2018@khu.ac.kr>
2018-08-25 21:15:15 +0900

    전체 게시판 검색 가능

---
+++ 06.everytime_keyword_bot/CONFIG.py
@@ -0,0 +1,6 @@
+ID = 'qqff0311'
+PW = 'kkjj1004'
+DELAY = 10
+TARGET_KEYWORD_LIST = ['수강신청']
+TARGET_KEYWORD = '수강신청'
+BOT_TOKEN = "602009043:AAHbtbRjehdhptkyxyxoyYbZdhd_RwYwyNw"
--- 06.everytime_keyword_bot/telegram_bot.py
+++ 06.everytime_keyword_bot/telegram_bot.py
@@ -2,45 +2,74 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import CONFIG
 import time
+import requests
+import telegram
 
+#변수
 ID = CONFIG.ID
 PW = CONFIG.PW
 TARGET_KEYWORD_LIST = CONFIG.TARGET_KEYWORD_LIST
-#찾고자 하는 키워드 추가
-keyword = input(">>알림받을 키워드를 입력해주세요 ::")
-TARGET_KEYWORD_LIST.append(keyword)
-
-#브라우저 제어
-browser = webdriver.Chrome('./chromedriver')
-browser.maximize_window()
-
-browser.get('https://khu.everytime.kr/370463')
-
-#아이디, 비밀번호 입력
-browser.find_element_by_name('userid').send_keys(ID)
-browser.find_element_by_name('password').send_keys(PW)
-
-#로그인 버튼 누르기
-browser.find_element_by_xpath('//*[@id="container"]/form/p[3]/input').click()
-
-#장터원룸 게시판 들어가기
-browser.get('https://khu.everytime.kr/370463')
-time.sleep(5)  # 페이지 글 로딩되는거 기다려주기
-
+TARGET_KEYWORD = CONFIG.TARGET_KEYWORD
 oldList = []
-while True:
-    #페이지 소스 얻어오기
-    bs4 = BeautifulSoup(browser.page_source, 'lxml')
-    print(TARGET_KEYWORD_LIST)
-    # 알림 부분 구현
-    articles = bs4.find_all('article')
-    for article in articles:
-        for target_keyword in TARGET_KEYWORD_LIST:
-            if target_keyword in article.h2.get_text().strip() and article.h2.get_text().strip() not in oldList:
-                print("[알림] :: ",article.h2.get_text().strip())
-                oldList.append(article.h2.get_text().strip())
 
-    #새로고침후 원하는 딜레이시간까지 기다리기
-    browser.get('https://khu.everytime.kr/370463')
-    time.sleep(CONFIG.DELAY)
-browser.quit()
+#봇 구현
+TOKEN = CONFIG.BOT_TOKEN
+BOT_URL = "https://api.telegram.org/bot{}/sendMessage?chat_id=-1001270337756".format(TOKEN)
+BOT = telegram.Bot(token = TOKEN) # bot 선언
+
+#찾고자 하는 키워드 추가 함수
+#def Add_keyword():
+#    keyword = input(">>알림받을 키워드를 입력해주세요 ::")
+#    TARGET_KEYWORD_LIST.append(keyword)
+
+if __name__ == '__main__':
+    #브라우저 제어
+    browser = webdriver.Chrome('./chromedriver')
+    #browser.maximize_window()
+
+    browser.get('https://khu.everytime.kr')
+    browser.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/form/a[1]').click()
+    #아이디, 비밀번호 입력
+    browser.find_element_by_name('userid').send_keys(ID)
+    browser.find_element_by_name('password').send_keys(PW)
+
+    #로그인 버튼 누르기
+    browser.find_element_by_xpath('//*[@id="container"]/form/p[3]/input').click()
+
+    # 검색하기
+    browser.find_element_by_xpath('//*[@id="container"]/div[3]/form/input').send_keys(TARGET_KEYWORD + '\n')
+    time.sleep(5)  # 페이지 글 로딩되는거 기다려주기
+
+    while True:
+        bs4 = BeautifulSoup(browser.page_source, 'lxml')
+        #print(TARGET_KEYWORD_LIST)
+        # 알림 부분 구현
+        articles = bs4.find_all('article')
+        for article in articles:
+            link = article.find('a',{'class':'article'})['href']
+            print(link)
+            for target_keyword in TARGET_KEYWORD_LIST:
+                if target_keyword in article.get_text().strip() and article.get_text().strip() not in oldList:
+                    if article.find('h2') != None: #제목 있을 때
+                        if len(article.p.get_text().strip()) >= 50: #글자수 제한
+                            #print("[제목] ::{}".format(article.h2.get_text().strip()))
+                            #print("[내용] ::{}".format(article.p.get_text().strip()))
+                            requests.get(BOT_URL + '&text=[새 글 알림]\n제목 ::{}\n내용 ::{}\nURL ::{}'.format(article.h2.get_text().strip(),article.p.get_text().strip()[:50]+'[더보기]','https://khu.everytime.kr'+link))
+                        else:
+                            requests.get(BOT_URL + '&text=[새 글 알림]\n제목 ::{}\n내용 ::{}\nURL ::{}'.format(article.h2.get_text().strip(),article.p.get_text().strip()[:50],'https://khu.everytime.kr'+link))
+
+
+                    else: #제목 없을 때
+                        if len(article.p.get_text().strip()) >= 50: #글자수 제한
+                            requests.get(BOT_URL + '&text=[새 글 알림]\n제목 ::없음\n내용 ::{}\nURL ::{}'.format(article.p.get_text().strip()[:50]+'[더보기]','https://khu.everytime.kr'+link))
+                        else:
+                            requests.get(BOT_URL + '&text=[새 글 알림]\n제목 ::없음\n내용 ::{}\nURL ::{}'.format(article.p.get_text().strip(),'https://khu.everytime.kr'+link))
+
+                        #print("[제목없음]") #제목 없을 때
+                        #print("[내용] ::{}".format(article.p.get_text().strip()))
+                oldList.append(article.get_text().strip())
+        #새로고침후 원하는 딜레이시간까지 기다리기
+        browser.get('https://khu.everytime.kr/search/all/{}'.format(target_keyword))
+        time.sleep(CONFIG.DELAY)
+
+    browser.quit()
